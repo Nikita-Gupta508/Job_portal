a8c083d7b6a91698a625e8c86dc8becabadaa175
Nikita Gupta <143890264+Nikita-Gupta508@users.noreply.github.com>
2025-09-06 12:27:16 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5176"],
+  origin: ["https://job-portal-pied-ten.vercel.app"],
   credentials: true,
 };
 
